e0b4c736d17ca4147b8409a5e444bfd73fc5eb6b
Mujjabi <60195025+Mujjabi@users.noreply.github.com>
2023-05-01 11:49:02 -0500

    Final R Code used in the phenotypic analysis

--- Final Analysis_AmesPaper.R
+++ Final Analysis_AmesPaper.R
@@ -133,7 +133,7 @@ write.csv(Ames_Blups, "Ames_Blups.csv")
 
 # Test for normality Assumptioms
 # qq plot
-#par(mfrow=c(2,3))
+par(mfrow=c(3,2))
 qqPlot(residuals(mod1), pch=19, col="dark blue", col.lines="red", xlab="Predicted quantiles", ylab="Observed quantiles", main = "Starch Content") 
 qqPlot(residuals(mod2), pch=19, col="dark blue", col.lines="red", xlab="Predicted quantiles", ylab="Observed quantiles", main = "Protein Content", id = FALSE) 
 qqPlot(residuals(mod3), pch=19, col="dark blue", col.lines="red", xlab="Predicted quantiles", ylab="Observed quantiles", main = "Oil Content", id = FALSE) 
@@ -204,32 +204,32 @@ Starch <- ggscatter(WisAmesCombined, x = "Starch", y = "Star_W",
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
           title = "Starch",
-          xlab = "Ames [%]", ylab = "WiDiv [%]")
+          xlab = "Observed [%]", ylab = "Literature [%]")
 
 Protein <- ggscatter(WisAmesCombined, x = "Protein", y = "Prot_W", 
                     add = "reg.line", conf.int = TRUE, 
                     cor.coef = TRUE, cor.method = "pearson",
                     title = "Protein",
-                    xlab = "Ames [%]", ylab = "WiDiv [%]")
+                    xlab = "Observed [%]", ylab = "Literature  [%]")
 
 Oil <- ggscatter(WisAmesCombined, x = "Oil", y = "Oil_W", 
                     add = "reg.line", conf.int = TRUE, 
                     cor.coef = TRUE, cor.method = "pearson",
                     title = "Oil",
-                 xlab = "Ames [%]", ylab = "WiDiv [%]")
+                 xlab = "Observed [%]", ylab = "Literature  [%]")
 
 Fiber <- ggscatter(WisAmesCombined, x = "Fib", y = "Fib_W", 
                     add = "reg.line", conf.int = TRUE, 
                     cor.coef = TRUE, cor.method = "pearson",
                     title = "Fiber",
-                    xlab = "Ames [%]", ylab = "WiDiv [%]")
+                    xlab = "Observed [%]", ylab = "Literature  [%]")
                   
 
 Ash <- ggscatter(WisAmesCombined, x = "Ash", y = "Ash_W", 
                     add = "reg.line", conf.int = TRUE, 
                     cor.coef = TRUE, cor.method = "pearson",
                     title = "Ash",
-                    xlab = "Ames [%]", ylab = "WiDiv [%]")
+                    xlab = "Observed [%]", ylab = "Literature  [%]")
 
 library(gridExtra)
 grid.arrange(Starch, Protein, Oil, Fiber,Ash, nrow = 3)
